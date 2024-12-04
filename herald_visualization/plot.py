259ebbc35c6f1c765bd66dc528ff003ee8e4de58
hancheng zhao <zhaohc@umich.edu>
2024-12-04 14:36:22 -0500

    add cycle dependence and ensure discharge start from 0 capacity

--- herald_visualization/plot.py
+++ herald_visualization/plot.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os, glob
+import numpy as np
 
-plt_params = {# 'axes.labelsize': 'x-large',
+default_params = {# 'axes.labelsize': 'x-large',
 #               'axes.titlesize': 'x-large',
 #               'xtick.labelsize': 'x-large',
 #               'ytick.labelsize': 'x-large',
@@ -28,14 +29,23 @@ plt_params = {# 'axes.labelsize': 'x-large',
               'legend.title_fontsize': 24,  # Increase legend title size if needed
               'legend.frameon': True
     }
-plt.rcParams.update(plt_params)
+
 
 def plot_discharge(
         dir_name,
         full_cycles = None,
         half_cycles = None,
+        save_png = True,
+        png_filename = None,
+        plt_params = None,
     ):
+    # plotting params
+    if plt_params != None:
+        plt.rcParams.update(plt_params)
+    else:
+        plt.rcParams.update(default_params)
     csv_files = glob.glob(os.path.join(dir_name,'outputs','*.csv'))
+    print(csv_files)
     summary_file = os.path.join(dir_name,'outputs','cycle_summary.csv')
     data_file = [file for file in csv_files if 'cycle_summary' not in file][0]
     df = pd.read_csv(data_file)
@@ -44,9 +54,29 @@ def plot_discharge(
     # only plot half cycles when half cycle is specified and full cycle is not specified
     if full_cycles == None and half_cycles == None:
         full_cycles = df_sum['full cycle'].tolist()
+    half_cycles = list(range(0,len(full_cycles)*2))
+    print(half_cycles,full_cycles)
     fig, ax = plt.subplots()
-    for cycle in full_cycles:
-        df1 = df[df['full cycle']==cycle]
+    colors = plt.cm.viridis(np.linspace(0,1,len(full_cycles)*2))
+    for i,cycle in enumerate(half_cycles[1:]):
+        df1 = df[df['half cycle']==cycle]
+        # round the whole df to 4 decimal places
+        df1 = df1.round(4)
         # remove 0 specific capacity
-        df
-    plt.plot(df)
+        df1 = df1[df1['Specific Capacity']!=0]
+        df1['Specific Capacity'] = df1['Specific Capacity'] - df1['Specific Capacity'].min()
+        # remove decreasing specific capacity
+        df1 = df1[df1['Specific Capacity'].cummax() == df1['Specific Capacity']]
+        if cycle % 2 == 1:
+            plt.plot(df1['Specific Capacity'],df1['Voltage'],color=colors[int(cycle)],label='Cycle '+str(int((cycle+1)/2)),linestyle='-')
+        else:
+            plt.plot(df1['Specific Capacity'],df1['Voltage'],color=colors[int(cycle-1)],linestyle='--')
+    plt.xlabel('Specific Capacity (mAh/g)')
+    plt.ylabel('Voltage (V)')
+    plt.legend(frameon=False)
+    plt.tight_layout()
+    if save_png:
+        if png_filename == None:
+            png_filename = os.path.join(dir_name,'outputs','cycle.png')
+        plt.savefig(png_filename,dpi=300)
+    return fig, ax
